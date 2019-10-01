903f25f97fb51681a8a2f048ca9bc5fcd3d9547b
Cody Standridge <standridge.cody@ou.edu>
2019-10-01 17:17:52 -0500

    reset has to be in 1st for loop and step has to be in second

--- run_test.py
+++ run_test.py
@@ -4,10 +4,7 @@ import gym_pacman
 import time
 import numpy as np
 from collections import defaultdict
-# TODO: I'm going push this in case you look at it before i do tomorrow. I'm mainly just confused about the order of
-#   of things right now because its like 4 am. I have everything to update the weights, but i still don't know how to
-#   do the gradient part. I'm sure you'll have questions about my haphazard code, but if you look at it in the morning
-#   try to do the update weights methods and just assume you have everything you need
+import gym_pacman.envs.util as util
 
 ###################################################
 #              Environment Setup                  #
@@ -62,25 +59,26 @@ def update_weights(s, a, r, s_prime):
 
     # TODO: create funtion feat(s,a) that returns a feature given s,a
     for weight in range(len(weight_vector)):
-        max_Q = max(q_table[s_prime])
-        correction = (r + gamma*max_Q) - q_table[s][a]
-        theta = feat(s,a)
+        max_q = max(q_table[s_prime])
+        correction = (r + gamma*max_q) - q_table[s][a]
+        theta = feat(s, a)
         weight_vector[weight] = weight_vector[weight] + alpha*correction*theta
 
 
 if __name__ == '__main__':
 
-    init_state = env.reset("mediumClassic.lay")
-    action = env.action_space.sample()
-
     # TODO: based on this algorithm
     #  http://www.cse.unsw.edu.au/~cs9417ml/RL1/algorithms.html
     for j in range(10000):  # 10000 episodes
-        state, r, done, info = env.step(action)
-
+        state = env.reset("mediumClassic.lay")
         for i in range(100):    # 100 steps (or until pacman dies or wins)
+            if util.flipCoin(epsilon):
+                action = np.argmax(q_table[state])
+            else:
+                action = env.action_space.sample()
             # TODO: choose a from s using policy derived from Q
             # TODO: take action a, observe r,s'
+            state, r, done, info = env.step(action)
             # TODO: update Q(s,a)
             # TODO: s <-- s'
             # TODO: repeat this process until s is teminal
@@ -92,10 +90,6 @@ if __name__ == '__main__':
             q_table[state][action] = float(np.dot(w_t, list(features.values())))
             # TODO: update the weights
             update_weights(state, action, r, s_prime)
-            # TODO: with probability epsilon take
-            #   action = argmax a of Q(s, a). argmax returns the argument a for which Q(s,a) is the largest
-            #   otherwise take a random action
-            action = env.action_space.sample()
             env.render()
             if done:
                 break
