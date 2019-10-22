6e38e00f543e223067fc42bc2b9a112eb2cf98cb
Ariana Paz Pirela <ariana-paz@hotmail.com>
2019-10-21 19:10:03 -0500

    remastered qlearning

--- qlearning.py
+++ qlearning.py
@@ -1,10 +1,10 @@
-from PIL import Image
 import gym
-import csv
 import numpy as np
 import matplotlib.pyplot as plot
 from collections import defaultdict
 import gym_pacman.envs.util as util
+
+# TODO: based on this algorithm http://www.cse.unsw.edu.au/~cs9417ml/RL1/algorithms.html
 
 
 ###################################################
@@ -13,6 +13,19 @@ import gym_pacman.envs.util as util
 env = gym.make('BerkeleyPacmanPO-v0')
 env.seed(1)
 done = False
+
+# episodes, steps, and rewards
+n_episodes = 5000
+n_steps = 100
+rewards = []
+
+# dictionary that will allow us to use the action name as the key
+q_table = defaultdict(lambda: [0.0, 0.0, 0.0, 0.0, 0.0])
+
+# Default values for learning algorithms
+alpha = 0.05    # smaller learning rates are better, more accurate over time
+gamma = 0.7     # high values give bigger weight to rewards
+epsilon = 0.3   # high epsilon values = more randomness
 
 
 ###################################################
@@ -29,18 +42,11 @@ state["power_pellets_timer"] = 0
 state["power_pellets_left"] = 0
 
 
-# dictionary that will allow us to use the action name as the key
-q_table = defaultdict(lambda: [0.0, 0.0, 0.0, 0.0, 0.0])
-
-
-# Default values for learning algorithms
-alpha = 0.05    # smaller learning rates are better, more accurate over time
-gamma = 0.9     # high values give bigger weight to rewards
-epsilon = 0.3   # high epsilon values = more randomness
-
-
+# TODO: need to fix this
+###################################################
+#                 Update States                   #
+###################################################
 def update_states():
-
     # update pacman's info
     state['pacman'] = {"x": info['curr_loc'][0],
                        "y": info['curr_loc'][1],
@@ -56,51 +62,20 @@ def update_states():
 
 
 ###################################################
-#                     Main                        #
+#              Save Weighted Graph                #
 ###################################################
-if __name__ == '__main__':
-    # TODO: based on this algorithm http://www.cse.unsw.edu.au/~cs9417ml/RL1/algorithms.html
-    episodes = []
-    rewards = []
-    for j in range(5000):  # 10000 episodes
-        gameState = env.reset("smallClassic.lay")
-
-        for i in range(100):    # 100 steps (or until pacman dies or wins)
-            # get action
-            if util.flipCoin(epsilon):
-                #TODO: this doesnt work with state as a deafult dict
-                action = np.argmax(q_table[gameState])
-                # print(action)
-            else:
-                action = env.action_space.sample()
-
-            # save old state and features
-            old_state = gameState
-            # old_features = list(state.values())
-
-            # s <-- s'
-            gameState, r, done, info = env.step(action)
-            # update_states()
-
-            max_action = max(q_table[gameState])
-            q_table[old_state][action] += alpha*((r + gamma*max_action) - q_table[old_state][action])
-            # env.render()
-            if done:
-                break
-        # save to csv file for graph
-        episodes.append(j)
-        rewards.append(info['episode']['r'])
-        print([str(j), str(info['episode']['r'])])
-
-    # moving average
+def moving_avg_graph(title, file_name):
+    # calculate weights
     weights = np.repeat(1.0, 100)/100
     moving_avg = np.convolve(rewards, weights, 'valid')
-    equalized_len = len(episodes) - len(moving_avg)
-    x = episodes[equalized_len:]
+    equalized_len = n_episodes - len(moving_avg)
+
+    # get the x and y points
+    x = np.arange(equalized_len, n_episodes)
     y = moving_avg
 
-    # scatter plots
-    plot.scatter(x, y, marker='*')
+    # scatter plot
+    plot.scatter(x, y, marker='.')
 
     # line of best fit
     z = np.polyfit(x, y, 1)
@@ -110,10 +85,56 @@ if __name__ == '__main__':
     # axis labels and title
     plot.xlabel('Episodes')
     plot.ylabel('Reward')
-    plot.title('5k Runs')
+    plot.title(title)
 
     # save figure as png
-    plot.savefig("plots_and_data/5k_run.png")
+    plot.savefig("plots_and_data/"+file_name)
+
+
+###################################################
+#               E Greedy                          #
+###################################################
+def policy(s):
+    if np.random.uniform(0, 1) < epsilon:
+        new_action = env.action_space.sample()
+    else:
+        new_action = np.argmax(q_table[s])
+
+    return new_action
+
+
+###################################################
+#                Q-learning                       #
+###################################################
+def learn(s, s_prime, r, a):
+    # TODO: this doesnt work with state as a default dict
+    max_action = max(q_table[s_prime])
+    current = q_table[s][a]
+    estimate = r + gamma * max_action
+    q_table[s][a] = current + alpha * (estimate - current)
+
+
+###################################################
+#                     Main                        #
+###################################################
+if __name__ == '__main__':
+    for episode in range(n_episodes):
+        state = env.reset("smallClassic.lay")
+        action = policy(state)
+
+        for i in range(n_steps):
+            # env.render()
+            state_prime, reward, done, info = env.step(action)
+            learn(state, state_prime, reward, action)
+
+            state = state_prime
+
+            if done:
+                break
+
+        rewards.append(info['episode']['r'])
+        print([str(episode), str(info['episode']['r'])])
+
+    moving_avg_graph(str(n_episodes)+'K Q-learning',
+                     str(n_episodes)+'K_q_learning')
     env.close()
-
-
